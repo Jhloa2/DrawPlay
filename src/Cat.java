f2b88e77e529914f444a395663b6164c22cccb02
Jordan Kincaid-Smith <Jordan Kincaid-Smith>
2016-02-22 20:28:28 -0700

    Made cat smaller, eyes wider, and changed eye color to black

--- src/Cat.java
+++ src/Cat.java
@@ -5,14 +5,14 @@ import java.awt.geom.*;
 public class Cat {
 	// drawing constants are private - noone needs to know what we're doing
 	// pick a head dimension
-	private static final int HEAD_DIMENSION = 1000;
+	private static final int HEAD_DIMENSION = 150;
 	// eyes will be about 1/4 from top of head and 1/4 from left
 	private static final int EYE_Y = HEAD_DIMENSION/4;
 	private static final int EYE_X = HEAD_DIMENSION/4;
 	private static final int EYE_SEPARATION = HEAD_DIMENSION/3;
 	// pick eye dimensions
 	private static final int EYE_HEIGHT = 35;
-	private static final int EYE_WIDTH = 10;
+	private static final int EYE_WIDTH = 60;
 	// pick mouth height, width is based on head dimension
 	private static final int MOUTH_HEIGHT = 60;
 	private static final int MOUTH_WIDTH = HEAD_DIMENSION/4;
@@ -30,7 +30,7 @@ public class Cat {
 		g2.setColor(Color.gray);
 		g2.fillOval(x, y, HEAD_DIMENSION, HEAD_DIMENSION);
 		// Draw the eyes
-		g2.setColor(Color.blue);
+		g2.setColor(Color.black);
 		x = catX + EYE_X; 
 		y = catY + EYE_Y;
 		g2.fillOval(x, y, EYE_WIDTH, EYE_HEIGHT);
